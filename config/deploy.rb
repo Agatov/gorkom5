3ed90de454135331154d205f6790d5f84beba236
agatov <agatovs@gmail.com>
2013-10-14 15:20:08 +0400

    title fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # Bundler tasks
 require 'bundler/capistrano'
 
-set :application, "courier"
-set :repository,  "git@github.com:Agatov/courier.git"
+set :application, "gorkom5"
+set :repository,  "git@github.com:Agatov/gorkom5.git"
 
 set :scm, :git
 
